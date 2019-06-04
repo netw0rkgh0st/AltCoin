37dcf2c9117b60a683675d2966e3cac3b84717d8
TFinch <travisfinch01@gmail.com>
2019-06-03 21:52:06 -0400

    Maturity Testing

--- src/main.cpp
+++ src/main.cpp
@@ -2474,11 +2474,17 @@ bool CheckTransaction(const CTransaction& tx, bool fZerocoinActive, bool fReject
                     assert(coins);
 
                     // If prev is coinbase, check that it's matured
-                    if (coins->IsCoinBase() || coins->IsCoinStake()) {
-                        if (nSpendHeight - coins->nHeight < PowPhase)
-                            return state.Invalid(
-                                error("CheckInputs() : tried to spend coinbase at depth %d, coinstake=%d", nSpendHeight - coins->nHeight, coins->IsCoinStake()),
-                                REJECT_INVALID, "bad-txns-premature-spend-of-coinbase");
+					if (coins->IsCoinBase() || coins->IsCoinStake()) {
+						if (chainActive.Height() = < Params().LAST_POW_BLOCK && nSpendHeight - coins->nHeight < Params().POW_MATURITY)
+							return state.Invalid(
+								error("CheckInputs() : tried to spend coinbase at depth %d, coinstake=%d", nSpendHeight - coins->nHeight, coins->IsCoinStake()),
+								REJECT_INVALID, "bad-txns-premature-spend-of-coinbase");
+					}else if (coins->IsCoinBase() || coins->IsCoinStake()) {
+						if (chainActive.Height() > Params().LAST_POW_BLOCK && nSpendHeight - coins->nHeight < Params().COINBASE_MATURITY) {
+							return state.Invalid(
+								error("CheckInputs() : tried to spend coinbase at depth %d, coinstake=%d", nSpendHeight - coins->nHeight, coins->IsCoinStake()),
+								REJECT_INVALID, "bad-txns-premature-spend-of-coinbase");
+						}
                     }
 
                     // Check for negative or overflow input values
