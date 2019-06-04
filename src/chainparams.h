aa6f57949835f326b6eef0d4da9276f90073ad3a
TFinch <travisfinch01@gmail.com>
2019-06-04 19:25:17 -0400

    Move to main.cpp

--- src/chainparams.h
+++ src/chainparams.h
@@ -77,13 +77,8 @@ public:
     int64_t Interval() const { return nTargetTimespan / nTargetSpacing; }
 
 	//Maturity Related Items
-    int COINBASE_MATURITY() const
-    {
-        if (chainActive.Height() <= nLastPOWBlock)
-			return nPoWMaturity; 
-		else
-			return nMaturity;
-    }
+	int COINBASE_MATURITY() const { return nMaturity;}
+	int POW_MATURITY() const { return nPoWMaturity; }
     unsigned int StakeMaturity() const { return nStakeMaturity; }
 
     CAmount MaxMoneyOut() const { return nMaxMoneyOut; }
