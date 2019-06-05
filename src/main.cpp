1f6029bc39ce16d022a36712f9ae71a17d377943
Travis <travisfinch01@gmail.com>
2019-06-05 09:48:27 -0400

    Revert "test2"
    This reverts commit 302c1c15d873f073c3254f4df10751257a733f56.

--- src/main.cpp
+++ src/main.cpp
@@ -2475,17 +2475,9 @@ bool CheckTransaction(const CTransaction& tx, bool fZerocoinActive, bool fReject
                     const CCoins* coins = inputs.AccessCoins(prevout.hash);
                     assert(coins);
 
-                    // If prev is coinbase, check that it's matured PoS Maturity
+                    // If prev is coinbase, check that it's matured
                     if (coins->IsCoinBase() || coins->IsCoinStake()) {
-                        if (nSpendHeight - coins->nHeight < Params().COINBASE_MATURITY())
-                            return state.Invalid(
-                                error("CheckInputs() : tried to spend coinbase at depth %d, coinstake=%d", nSpendHeight - coins->nHeight, coins->IsCoinStake()),
-                                REJECT_INVALID, "bad-txns-premature-spend-of-coinbase");
-                    }
-
-					                    // If prev is coinbase, check that it's matured from PoW Maturity
-                    if (coins->IsCoinBase() || coins->IsCoinStake()) {
-                        if (nSpendHeight - coins->nHeight < Params().POW_MATURITY())
+                        if (nSpendHeight - coins->nHeight < Params().COINBASE_MATURITYv2())
                             return state.Invalid(
                                 error("CheckInputs() : tried to spend coinbase at depth %d, coinstake=%d", nSpendHeight - coins->nHeight, coins->IsCoinStake()),
                                 REJECT_INVALID, "bad-txns-premature-spend-of-coinbase");
