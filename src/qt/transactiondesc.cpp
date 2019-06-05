9cf0d919a8eaaf8ff0455390f7714e29fc2e5b96
Travis <travisfinch01@gmail.com>
2019-06-05 14:38:48 -0400

    Revert "a"
    This reverts commit f6723707506c8afa771430bd34a2adadba14727b.

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -279,7 +279,6 @@ QString TransactionDesc::toHTML(CWallet* wallet, CWalletTx& wtx, TransactionReco
 				quint32 numBlocksToMaturity = Params().POW_MATURITY() + 1;
 				strHTML += "<br>" + tr("Generated coins must mature %1 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.").arg(QString::number(numBlocksToMaturity)) + "<br>";
         } else if (chainActive.Height() > Params().LAST_POW_BLOCK()) {
-            if (wtx.IsCoinBase()) 
             quint32 numBlocksToMaturity = Params().COINBASE_MATURITY() + 1;
             strHTML += "<br>" + tr("Generated coins must mature %1 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.").arg(QString::number(numBlocksToMaturity)) + "<br>";
 		}
