9871603c8769d0da87eb4766eeae1d3abb388861
TFinch <travisfinch01@gmail.com>
2019-06-05 21:38:58 -0400

    Revert "why"
    This reverts commit cc200689fe0fe209b8f042a6668dd06b39b783b6.

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -487,10 +487,10 @@ void CTxMemPool::removeCoinbaseSpends(const CCoinsViewCache* pcoins, unsigned in
             std::map<uint256, CTxMemPoolEntry>::const_iterator it2 = mapTx.find(txin.prevout.hash);
             if (it2 != mapTx.end())
                 continue;
-			const CCoins* coins = pcoins->AccessCoins(txin.prevout.hash);
-		if (fSanityCheck) assert(coins);
+            const CCoins* coins = pcoins->AccessCoins(txin.prevout.hash)
+				if (fSanityCheck) assert(coins);
 			if ((chainActive.Height() <= Params().LAST_POW_BLOCK(){					
-				fif (!coins || ((coins->IsCoinBase() || coins->IsCoinStake()) && nMemPoolHeight - coins->nHeight < (unsigned)Params().POW_MATURITY())) 
+				if (!coins || ((coins->IsCoinBase() || coins->IsCoinStake()) && nMemPoolHeight - coins->nHeight < (unsigned)Params().POW_MATURITY())) 
 							transactionsToRemove.push_back(tx);
 							break;
             } else if((chainActive.Height() > Params().LAST_POW_BLOCK()){
